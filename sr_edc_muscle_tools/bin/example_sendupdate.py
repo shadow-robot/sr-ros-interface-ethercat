c13b4f0ae15de4cf63a7b7f1978c23fa230a8500
Shadow Hand <hand@hand.(none)>
2013-08-14 20:42:24 +0900

    Fix import

--- sr_edc_muscle_tools/bin/example_sendupdate.py
+++ sr_edc_muscle_tools/bin/example_sendupdate.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import roslib; roslib.load_manifest('sr_naist_living_machines')
+import roslib; roslib.load_manifest('sr_edc_muscle_tools')
 import rospy
 from sr_robot_msgs.msg import sendupdate, joint
 from std_msgs.msg import Float64
