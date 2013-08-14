612621eb11d4f2ad1f54d0f4d5316029f5b17912
Shadow Hand <hand@hand.(none)>
2013-08-14 22:12:42 +0900

    Add the pressure calib script

---
+++ sr_edc_muscle_tools/bin/calibrate_pressure_sensors.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DURATION=6000
+
+for muscle in ffj0 ffj3 ffj4; do #rfj0 rfj3 rfj4 thj2 thj3 thj4 thj5 wrj1 wrj2; do
+rostopic pub -1 /sh_"$muscle"_muscle_valve_controller/command sr_robot_msgs/JointMuscleValveControllerCommand "cmd_valve_muscle: [4, -4]
+cmd_duration_ms: [$DURATION,$DURATION]"
+echo $muscle >> foo.out
+rostopic echo -n1 /sh_"$muscle"_muscle_valve_controller/state/muscle_pressure_0 >> foo.out
+rostopic pub -1 /sh_"$muscle"_muscle_valve_controller/command sr_robot_msgs/JointMuscleValveControllerCommand "cmd_valve_muscle: [-4, 4]
+cmd_duration_ms: [$DURATION,$DURATION]"
+rostopic echo -n1 /sh_"$muscle"_muscle_valve_controller/state/muscle_pressure_1 >> foo.out
+rostopic pub -1 /sh_"$muscle"_muscle_valve_controller/command sr_robot_msgs/JointMuscleValveControllerCommand "cmd_valve_muscle: [-4, -4]
+cmd_duration_ms: [$DURATION,$DURATION]"
+
+done
+exit
