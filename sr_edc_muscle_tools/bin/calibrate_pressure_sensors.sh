529149da6b5886d3b1786af5ebf371c9d9b6e54a
Shadow Hand <hand@hand.(none)>
2013-08-22 00:23:53 +0900

    add extra joints to calibrate_pressure_sensors, remove ampersand from end of empty extend and flex publish commands to ensure clean return to command line

--- sr_edc_muscle_tools/bin/calibrate_pressure_sensors.sh
+++ sr_edc_muscle_tools/bin/calibrate_pressure_sensors.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-DURATION=6000
+DURATION=20000
 
-for muscle in ffj0 ffj3 ffj4; do #rfj0 rfj3 rfj4 thj2 thj3 thj4 thj5 wrj1 wrj2; do
+for muscle in ffj0 ffj3 ffj4 rfj0 rfj3 rfj4 thj2 thj3 thj4 thj5 wrj1 wrj2; do
 rostopic pub -1 /sh_"$muscle"_muscle_valve_controller/command sr_robot_msgs/JointMuscleValveControllerCommand "cmd_valve_muscle: [4, -4]
 cmd_duration_ms: [$DURATION,$DURATION]"
 echo $muscle >> foo.out
+sleep 20
 rostopic echo -n1 /sh_"$muscle"_muscle_valve_controller/state/muscle_pressure_0 >> foo.out
+
 rostopic pub -1 /sh_"$muscle"_muscle_valve_controller/command sr_robot_msgs/JointMuscleValveControllerCommand "cmd_valve_muscle: [-4, 4]
 cmd_duration_ms: [$DURATION,$DURATION]"
+sleep 20
 rostopic echo -n1 /sh_"$muscle"_muscle_valve_controller/state/muscle_pressure_1 >> foo.out
+
 rostopic pub -1 /sh_"$muscle"_muscle_valve_controller/command sr_robot_msgs/JointMuscleValveControllerCommand "cmd_valve_muscle: [-4, -4]
 cmd_duration_ms: [$DURATION,$DURATION]"
 
--- sr_edc_muscle_tools/bin/flex.sh
+++ sr_edc_muscle_tools/bin/flex.sh
@@ -14,6 +14,6 @@ fi
 DURATION=$2
 
 rostopic pub -1 /sh_"$1"_muscle_valve_controller/command sr_robot_msgs/JointMuscleValveControllerCommand "cmd_valve_muscle: [4, -4]
-cmd_duration_ms: [$DURATION,$DURATION]" &
+cmd_duration_ms: [$DURATION,$DURATION]"
 
 exit
