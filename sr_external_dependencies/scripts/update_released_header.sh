1266a4df44adac1a2b58d54dd71b51aa53ce8f94
Toni Oliver <toni.oliver.d@gmail.com>
2013-12-19 16:03:54 +0000

    Fixing update_released_header script

--- sr_external_dependencies/scripts/update_released_header.sh
+++ sr_external_dependencies/scripts/update_released_header.sh
@@ -12,10 +12,8 @@ echo "copying (in directory: " $START_PATH " ), from: ../released/external/* to 
 cp -r ../released/external/* ../include/sr_external_dependencies/external/
 chmod -R a-w ../include/sr_external_dependencies/external/
 
-#create the folder in case it doesn't exist
-mkdir -p ../compiled_firmware/released_firmware/
 
-echo "copying (in directory: " $START_PATH " ), from: ../released/released_firmware/* to ../compiled_firmware/released_firmware/"
-chmod -R a+w ../include/sr_external_dependencies/external/
-cp -r ../released/released_firmware/* ../compiled_firmware/released_firmware/
-chmod -R a-w ../include/sr_external_dependencies/external/
+echo "copying (in directory: " $START_PATH " ), from: ../released/released_firmware to ../compiled_firmware"
+chmod -R a+w ../compiled_firmware
+cp -r ../released/released_firmware ../compiled_firmware
+chmod -R a-w ../compiled_firmware
