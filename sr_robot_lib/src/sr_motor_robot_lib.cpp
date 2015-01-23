dbc949882fb95205e60fdaa394fafa8633e5eb09
Toni Oliver <toni.oliver.d@gmail.com>
2015-01-23 12:05:40 +0000

    Skip reset gains for arm controllers

--- sr_robot_lib/src/sr_motor_robot_lib.cpp
+++ sr_robot_lib/src/sr_motor_robot_lib.cpp
@@ -1107,6 +1107,9 @@ bool SrMotorRobotLib<StatusType, CommandType>::change_control_parameters(int16_t
           continue;
         if (controllers_list.response.controller[i].name.find("trajectory_controller") != std::string::npos)
           continue;
+        if ((controllers_list.response.controller[i].name.find("ra_sr_ur_controller") != std::string::npos) ||
+            (controllers_list.response.controller[i].name.find("la_sr_ur_controller") != std::string::npos))
+          continue;
         ros::ServiceClient reset_gains_client = nh.template serviceClient<std_srvs::Empty>(controllers_list.response.controller[i].name + "/reset_gains");
         std_srvs::Empty empty_message;
         if (!reset_gains_client.call(empty_message))
