94fb3ebb712fd925e7f47a5b0c09ba52a7ea46e6
Andriy Petlovanyy <AndriyPt@users.noreply.github.com>
2015-11-30 21:08:08 +0200

    Simplified structure of the header files

--- sr_external_dependencies/scripts/make_release.sh
+++ sr_external_dependencies/scripts/make_release.sh
@@ -15,15 +15,18 @@ cp -r compiled_firmware/* released/
 chmod -R a-w released/*
 
 #TODO: write the current versions of the host software / firmware in a file.
-VERS_COMMON=`svnversion include/sr_external_dependencies/external/common/`
-VERS_0220=`svnversion include/sr_external_dependencies/external/0220_palm_edc/`
-VERS_0230_TS=`svnversion include/sr_external_dependencies/external/0230_palm_edc_TS/`
-VERS_0320_MUSCLE=`svnversion include/sr_external_dependencies/external/0320_palm_edc_muscle/`
-VERS_BOOTLD=`svnversion include/sr_external_dependencies/external/simplemotor-bootloader/`
-VERS_FW=`svnversion compiled_firmware/released_firmware/`
+pushd /tmp
+rm -rf ./firmware
+git clone https://github.com/shadow-robot/hand-firmware.git firmware
+
+VERS_GIT_FIRMWARE=`git --git-dir /tmp/firmware/.git log --pretty=format:%h -n 1`
 VERS_GIT_ETHERCAT=`git --git-dir $(dirname $(rosstack find shadow_robot_ethercat))/.git log --pretty=format:%h -n 1`
 VERS_GIT_SHADOW=`git --git-dir $(dirname $(rosstack find shadow_robot))/.git log --pretty=format:%h -n 1`
 
-echo "protocol_common:"${VERS_COMMON} "; protocol_motor:"${VERS_0220} "; protocol_muscle:"${VERS_0320_MUSCLE} "; protocol_ubi_ts:"${VERS_0230_TS} "; bootloader:"${VERS_BOOTLD}" ; firmware:"${VERS_FW}" ; git_shadow:"${VERS_GIT_SHADOW} "; git_ethercat:"${VERS_GIT_ETHERCAT} > tested_version.yaml
+rm -rf ./firmware
 
-git tag --force "protocol_common_"${VERS_COMMON}"__protocol_motor_"${VERS_0220}"__protocol_muscle_"${VERS_0320_MUSCLE}"__protocol_ubi_ts_"${VERS_0230_TS}"__bootloader_"${VERS_BOOTLD}"__firmware_"${VERS_FW}"__git_shadow_"${VERS_GIT_SHADOW}"__git_ethercat_"${VERS_GIT_ETHERCAT}
+popd
+
+echo "git_firmware:"${VERS_GIT_FIRMWARE}" ; git_shadow:"${VERS_GIT_SHADOW} "; git_ethercat:"${VERS_GIT_ETHERCAT} > tested_version.yaml
+
+git tag --force "git_firmware:"${VERS_GIT_FIRMWARE}"__git_shadow_"${VERS_GIT_SHADOW}"__git_ethercat_"${VERS_GIT_ETHERCAT}
--- sr_external_dependencies/scripts/update_header.sh
+++ sr_external_dependencies/scripts/update_header.sh
@@ -1,44 +1,45 @@
 #!/bin/bash
 
+TEMP_GIT_REPO_PATH=/tmp/firmware
 PREVIOUS_PATH=`pwd`
 START_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${START_PATH}
 
+chmod -R a+w ../include/sr_external_dependencies/external
+rm -rf ../include/sr_external_dependencies/external
 mkdir -p ../include/sr_external_dependencies/external
 cd "../include/sr_external_dependencies/external"
 
-function checkout_or_update {
+function copy_released_files {
     DIR_NAME=${1}
-    SVN_PATH=${2}
-    if [ -d ${DIR_NAME} ] ; then
-	chmod -R a+w ${DIR_NAME}
-	pushd ${DIR_NAME}
-	echo "Upgrading ${DIR_NAME}"
-	svn upgrade
-	echo "Updating ${DIR_NAME}"
-	svn up
-	popd
+    GIT_PATH=${TEMP_GIT_REPO_PATH}/${2}
+
+	echo "Updating ${DIR_NAME} from ${GIT_PATH}"
+	mkdir -p ${DIR_NAME}
+	cp -R ${GIT_PATH}/*.h ${GIT_PATH}/*.hpp ${GIT_PATH}/*.hex ${GIT_PATH}/*.txt ${DIR_NAME}
+	rm -f ${DIR_NAME}/this_node.h
 
     #removes write access
 	chmod -R a-w ${DIR_NAME}
-    else
-	chmod -R a+w .
-	echo "Checking out ${SVN_PATH} in ${DIR_NAME}"
-	svn co ${SVN_PATH} ${DIR_NAME}
-
-    #removes write access
-	chmod -R a-w ${DIR_NAME}
-	chmod -R a-w .
-    fi
 }
 
-checkout_or_update common svn://thoth:9998/Pic32/trunk/nodes/common
-checkout_or_update 0220_palm_edc svn://thoth:9998/Pic32/trunk/nodes/0220_palm_edc
-checkout_or_update 0320_palm_edc_muscle svn://thoth:9998/Pic32/trunk/nodes/0320_palm_edc_muscle
-checkout_or_update 0230_palm_edc_TS svn://thoth:9998/Pic32/trunk/nodes/0230_palm_edc_TS
-checkout_or_update simplemotor-bootloader svn://thoth:9998/EDC/simplemotor-bootloader/
+rm -rf ${TEMP_GIT_REPO_PATH}
+git clone https://github.com/shadow-robot/hand-firmware.git ${TEMP_GIT_REPO_PATH}
+
+copy_released_files common PIC32/nodes/common
+copy_released_files 0220_palm_edc PIC32/nodes/0220_palm_edc
+copy_released_files 0320_palm_edc_muscle PIC32/nodes/0320_palm_edc_muscle
+copy_released_files 0230_palm_edc_TS PIC32/nodes/0230_palm_edc_TS
+copy_released_files simplemotor-bootloader EDC/simplemotor-bootloader
 
 cd ${START_PATH}
-cd "../compiled_firmware"
-checkout_or_update released_firmware svn://thoth:9998/EDC/simplemotor/released_firmware/
+cd ".."
+chmod -R a+w ./released_firmware
+rm -rf ./released_firmware
+copy_released_files released_firmware EDC/simplemotor/released_firmware
+
+rm -rf ${TEMP_GIT_REPO_PATH}
+
 cd ${PREVIOUS_PATH}
+
+echo "Firmware update finished"
