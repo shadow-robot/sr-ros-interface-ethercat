c022edb24115baa591a707a0231b66ca87593574
Andriy Petlovanyy <AndriyPt@users.noreply.github.com>
2015-12-02 13:25:09 +0200

    Version of firmware is read from release tags

--- sr_external_dependencies/scripts/make_release.sh
+++ sr_external_dependencies/scripts/make_release.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-START_PATH=`pwd`
-
-#We copy the released protocol files to the correct directory
+PREVIOUS_PATH=`pwd`
+START_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${START_PATH}
 
 #TODO: write the current versions of the host software / firmware in a file.
@@ -13,13 +12,15 @@ rm -rf /tmp/sr_core
 git clone https://github.com/shadow-robot/hand-firmware.git /tmp/firmware
 if [ $? -ne 0 ]; then
     echo "Error: Could not clone hand-firmware repository. Exiting script!"
+    cd ${PREVIOUS_PATH}
     exit 1
 fi
 
 git clone https://github.com/shadow-robot/sr_common.git /tmp/sr_common
 git clone https://github.com/shadow-robot/sr_core.git /tmp/sr_core
 
-VERS_GIT_FIRMWARE=`git --git-dir /tmp/firmware/.git log --pretty=format:%h -n 1`
+VERS_GIT_FIRMWARE=`git --git-dir /tmp/firmware/.git tag | tail -1`
+
 VERS_GIT_COMMON=`git --git-dir /tmp/sr_common/.git log --pretty=format:%h -n 1`
 VERS_GIT_CORE=`git --git-dir /tmp/sr_core/.git log --pretty=format:%h -n 1`
 VERS_GIT_ETHERCAT=`git --git-dir $(dirname $(rosstack find shadow_robot_ethercat))/.git log --pretty=format:%h -n 1`
@@ -29,6 +30,8 @@ rm -rf /tmp/firmware
 rm -rf /tmp/sr_common
 rm -rf /tmp/sr_core
 
-echo "git_firmware:"${VERS_GIT_FIRMWARE}" ; git_sr_common:"${VERS_GIT_COMMON}" ; git_sr_core:"${VERS_GIT_CORE}" ; git_shadow:"${VERS_GIT_SHADOW}"; git_ethercat:"${VERS_GIT_ETHERCAT} > tested_version.yaml
+echo "git_firmware:"${VERS_GIT_FIRMWARE}" ; git_sr_common:"${VERS_GIT_COMMON}" ; git_sr_core:"${VERS_GIT_CORE}" ; git_shadow:"${VERS_GIT_SHADOW}"; git_ethercat:"${VERS_GIT_ETHERCAT} > ../tested_version.yaml
 
 git tag --force "git_firmware_"${VERS_GIT_FIRMWARE}"__git_sr_common_"${VERS_GIT_COMMON}"__git_sr_core_"${VERS_GIT_CORE}"__git_shadow_"${VERS_GIT_SHADOW}"__git_ethercat_"${VERS_GIT_ETHERCAT}
+
+cd ${PREVIOUS_PATH}
