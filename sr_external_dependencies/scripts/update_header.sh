993637e827bbdb4c56d9a980ae9c33bb70e43e27
Andriy Petlovanyy <AndriyPt@users.noreply.github.com>
2015-12-15 12:50:33 +0200

    Removed archived attribute from header files and firmware

--- sr_external_dependencies/scripts/update_header.sh
+++ sr_external_dependencies/scripts/update_header.sh
@@ -5,7 +5,6 @@ PREVIOUS_PATH=`pwd`
 START_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${START_PATH}
 
-chmod -R a+w ../include/sr_external_dependencies/external
 rm -rf ../include/sr_external_dependencies/external
 mkdir -p ../include/sr_external_dependencies/external
 cd "../include/sr_external_dependencies/external"
@@ -18,9 +17,6 @@ function copy_released_files {
 	mkdir -p ${DIR_NAME}
 	cp -Rf ${GIT_PATH}/*.h ${GIT_PATH}/*.hpp ${GIT_PATH}/*.hex ${GIT_PATH}/*.txt ${DIR_NAME}
 	rm -f ${DIR_NAME}/this_node.h
-
-    #removes write access
-	chmod -R a-w ${DIR_NAME}
 }
 
 rm -rf ${TEMP_GIT_REPO_PATH}
@@ -38,7 +34,6 @@ copy_released_files simplemotor-bootloader EDC/simplemotor-bootloader
 
 cd ${START_PATH}
 cd ".."
-chmod -R a+w ./released_firmware
 copy_released_files released_firmware EDC/simplemotor/released_firmware
 
 rm -rf ${TEMP_GIT_REPO_PATH}
